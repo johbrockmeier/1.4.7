7fee181337c54d72281e12e089fe5a72998ce991
johbrockmeier <johbrockmeier@kearneycats.com>
2017-03-02 10:25:11 -0600

    Logo Place Function
    Function that will set the location of the logo onto a given image.

--- 1.4.7 JohnBrockmeier_MattJamison.py
+++ 1.4.7 JohnBrockmeier_MattJamison.py
@@ -2,3 +2,24 @@ import PIL
 import matplotlib.pyplot as plt
 import os.path  
 import PIL.ImageDraw            
+
+def logo_place(image, side):
+    image_width, image_height = image.size
+    logo.resize(image_width * .125, image_height *.125)
+    border_spacing = 0.1
+    if side == 'upper right' or 'upper left':
+        logo_y = (image_height - (image_height * border_spacing))
+        if side == 'upper right':
+            logo_x = (image_width - (image_width * border_spacing))
+        else:
+            logo_x = image_width * border_spacing
+    else:
+        logo_y = image_height * border_spacing
+        if side == 'lower right':
+            logo_x = (image_width - (image_width * border_spacing))
+        else:
+            logo_x = image_width * border_spacing
+    image.paste(logo, (logo_x, logo_y))
+    return image 
+            
+    
