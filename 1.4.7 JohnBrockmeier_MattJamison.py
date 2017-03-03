608ff3ebe49fcf8187ae379dbf734670d401f95e
johbrockmeier <johbrockmeier@kearneycats.com>
2017-03-03 10:00:09 -0600

    Added Comments and DocStrings

--- 1.4.7 JohnBrockmeier_MattJamison.py
+++ 1.4.7 JohnBrockmeier_MattJamison.py
@@ -6,6 +6,7 @@ import PIL.ImageDraw
 def get_images(directory=None):
     if directory == None:
         directory = os.getcwd() # Use working directory if unspecified
+    # create 2 new lists for images and files
     image_list=[] 
     file_list = []
     directory_list = os.listdir(directory) 
@@ -36,6 +37,11 @@ def set_logo(side, directory=None):
         new_image.save(new_image_filename)
         
 def test():
+    ''' 
+    Will test the program in order to see if using proper directories and
+    image control. If test passes: directory has been created and image has been
+    saved as a .png
+    '''
     for a in range(1):
         directory = os.getcwd()
         #set_logo('left')
@@ -52,11 +58,15 @@ def test():
     except OSError:
         pass
          
-def logo_place(image, side):
+def logo_place(image, side): #Places the logo onto specified corner of an image
+    ''' 
+    This function will place a predetermined logo in a specified corner of the image, or in the middle.
+    Use middle or upper/lower then left/right to set location of logo on the image.
+    ''' 
     image_width, image_height = image.size
     logo.resize(image_width * .125, image_height *.125)
     border_spacing = 0.1
-    if side == 'upper right' or 'upper left':
+    if side == 'upper right' or 'upper left': # check to see where user wants logo
         logo_y = (image_height - (image_height * border_spacing))
         if side == 'upper right':
             logo_x = (image_width - (image_width * border_spacing))
@@ -68,7 +78,6 @@ def logo_place(image, side):
             logo_x = (image_width - (image_width * border_spacing))
         else:
             logo_x = image_width * border_spacing
+    # paste image according to conditions inputed by user and computed by program
     image.paste(logo, (logo_x, logo_y))
     return image 
-            
-  
